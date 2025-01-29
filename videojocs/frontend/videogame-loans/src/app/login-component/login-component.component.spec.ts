b2effd568760cbec1568546b6068b8b2923de724
OctagonalDefence <eromero110@boscdelacoma.cat>
2025-01-29 17:33:50 +0100

    create the interface test fail

--- videojocs/frontend/videogame-loans/src/app/login-component/login-component.component.spec.ts
+++ videojocs/frontend/videogame-loans/src/app/login-component/login-component.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponentComponent } from './login-component.component';
+import e from 'express';
 
 describe('LoginComponentComponent', () => {
   let component: LoginComponentComponent;
@@ -20,4 +21,13 @@ describe('LoginComponentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create the interface', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Login');
+    expect(compiled.querySelector('label').textContent).toContain('Usuari');
+    expect(compiled.querySelector('label').textContent).toContain('Contrassenya');
+    expect(compiled.querySelector('button').textContent).toContain('Entrar');    
+  });
+
 });
