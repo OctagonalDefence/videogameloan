3824bae0a8529c0cb46a12010ef1ba26dcd45173
OctagonalDefence <eromero110@boscdelacoma.cat>
2025-01-30 19:13:47 +0100

    service getAllUsers test fail

--- videojocs/frontend/videogame-loans/src/app/services/auth.service.spec.ts
+++ videojocs/frontend/videogame-loans/src/app/services/auth.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AuthService', () => {
   let service: AuthService;
+  
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers : [provideHttpClient()]
+
+    });
     service = TestBed.inject(AuthService);
+    
+    
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+   
+  it('should have the getAllUsers method', () => {
+    expect(service.getAllUsers).toBeDefined();
+  });
+
+  it('getAllUsers should retrieve all users from the database', () => {
+    expect(service.getAllUsers()).toBeTruthy();    
+  });
+
+
 });
--- videojocs/frontend/videogame-loans/src/app/services/auth.service.ts
+++ videojocs/frontend/videogame-loans/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const dbUrl = 'http://localhost:3000/api';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  
+
+
+
 }
