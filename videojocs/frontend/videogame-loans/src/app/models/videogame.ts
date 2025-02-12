cdad333ad5daefcc3c8c577c86e4095d2b933393
OctagonalDefence <eromero110@boscdelacoma.cat>
2025-02-12 19:10:10 +0100

    model tests success

--- videojocs/frontend/videogame-loans/src/app/models/videogame.ts
+++ videojocs/frontend/videogame-loans/src/app/models/videogame.ts
@@ -1,5 +1,10 @@
 export class Videogame {
     
+    nom: string = '';
+    any: number = 0;
+    plataforma: string = '';
+    desenvolupadora: string = '';
+    unitats: number = 0;
 
     constructor(values: Object = {}) {
         Object.assign(this, values);
