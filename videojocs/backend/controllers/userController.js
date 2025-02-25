764a54bd5dd2c2d2558c7d872afb29f094ab6c0d
OctagonalDefence <eromero110@boscdelacoma.cat>
2025-02-25 19:44:50 +0100

    login API method fix

--- videojocs/backend/controllers/userController.js
+++ videojocs/backend/controllers/userController.js
@@ -2,6 +2,7 @@ import sql from 'mssql';
 import dbConfig from '../config/dbConfig.js';
 import errorHandler from '../middleware/errorHandler.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const getAllUsers = async (req, res) => {
     try {
@@ -20,7 +21,7 @@ export const login = async (req, res) => {
         let pool = await sql.connect(dbConfig);
         let result = await pool.request()
             .input('username', sql.NVarChar, username)
-            .query('SELECT * FROM Usuari WHERE Email = @username');
+            .query('SELECT Email, Password FROM Usuari WHERE Email = @username');
 
         if (result.recordset.length === 0) {
             return res.status(401).json({ error: 'Invalid username or password' });
@@ -28,17 +29,18 @@ export const login = async (req, res) => {
 
         const user = result.recordset[0];
 
-        if (!user.password || !bcrypt.compareSync(password, user.password)) {
+        console.log('Password:', password);
+        console.log('User Password:', user.Password);
+
+        const isPasswordValid = await bcrypt.compare(password, user.Password);
+        if (!isPasswordValid) {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
 
-        const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
+        const token = jwt.sign({ username: user.Email }, process.env.JWT_SECRET);
         return res.json({ token });
 
     } catch (error) {
         errorHandler(error, req, res);
     }
 };
-
-
-
